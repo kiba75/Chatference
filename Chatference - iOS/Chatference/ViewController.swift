09ea1d93db8588b4f3995c6a923994068d87f6be
Ryno Claassen <rynoclaassen@gmail.com>
2018-10-25 10:07:10 +0200

    initial commit

---
+++ Chatference - iOS/Chatference/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatference
+//
+//  Created by Ryno Claassen on 2018/10/25.
+//  Copyright © 2018 Ryno Claassen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
