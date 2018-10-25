c349fe6860cc358b68a51b87ed2d29683aafbeb5
Pieter Smit <pieter@polymorph.co.za>
2018-10-25 12:23:15 +0200

    Add Basic Firebase Integration

--- Chatference - Android/app/src/main/java/za/co/polymorph/chatference/MainActivity.java
+++ Chatference - Android/app/src/main/java/za/co/polymorph/chatference/MainActivity.java
@@ -3,11 +3,25 @@ package za.co.polymorph.chatference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //database = FirebaseDatabase.getInstance().getReference();
+        //writeNewUser(database.push().getKey(), "Pieter", "pieter@polymorph.co.za");
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        database.child("users").child(userId).setValue(user);
     }
 }
